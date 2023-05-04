5c543a4d493c27d2f742334229d11a5717d84254
Renan <renanbrandaorj@gmail.com>
2023-05-04 18:11:39 -0300

    TaskList Completo

--- src/routes/Task.js
+++ src/routes/Task.js
@@ -2,6 +2,7 @@ const express = require('express');
 const checklistDependentRoute = express.Router();
 const Checklist = require('../models/checklist');
 const Task = require('../models/task');
+const simpleRouter = express.Router();
 
 checklistDependentRoute.get("/:id/tasks/new", async (req,res)=>{
     try{
@@ -12,15 +13,28 @@ checklistDependentRoute.get("/:id/tasks/new", async (req,res)=>{
     }
 })
 
+simpleRouter.delete("/:id", async (req, res)=>{
+    try{
+        let task = await Task.findByIdAndDelete(req.params.id);
+        let checklist = await Checklist.findById(task.checklist);
+        let taskToRemove = checklist.tasks.indexOf(task);
+        checklist.tasks.slice(taskToRemove, 1);
+        await checklist.save();
+        res.redirect(`/checklists/${checklist._id}`)
+    }catch(error){
+        res.status(422).render('Pages/error', {errors: 'Erro ao remover uma tarefa'})
+    }
+})
+
 checklistDependentRoute.post("/:id/tasks", async (req, res)=>{
     let {name} = req.body.task;
     let task = new Task({name, checklist: req.params.id})
     try{
         await task.save();
         let checklist = await Checklist.findById(req.params.id);
-        checklist.task.push(task);
-        checklist.save();
-        res.redirect(`checklists/${req.params.id}`);
+        checklist.tasks.push(task);
+        await checklist.save();
+        res.redirect(`/checklists/${req.params.id}`);
     }catch(error){
         let errors = error.errors;
         res.status(422).render('tasks/new', {task: {...task, errors}, checklistId: req.params.id})
@@ -29,4 +43,7 @@ checklistDependentRoute.post("/:id/tasks", async (req, res)=>{
 
 
 
-module.exports = {checklistDependent: checklistDependentRoute};
+module.exports = {
+    checklistDependent: checklistDependentRoute,
+    simple: simpleRouter
+    };
